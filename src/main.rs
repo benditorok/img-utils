9d33ffb9f6ad04d4f961a96ea75edcf9da972597
Bendegúz Török <bendi.torok@gmail.com>
2024-09-24 20:40:28 +0200

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,24 @@
-use image::EncodableLayout;
-use libloading::{Library, Symbol};
-use std::{env, ffi::c_void, path::Path};
-
-// Define the signature for the processImage function
-type InvertImageFn = unsafe extern "C" fn(image: *mut u8, image_len: u32, width: u32, height: u32);
+use libloading::Library;
+use std::{env, path::Path};
 
 fn main() -> anyhow::Result<()> {
+    // Load the libcudaimg library
     let lib_path = Path::new(env!("OUT_DIR")).join("data/libcudaimg.dll");
+    let libcudaimg = unsafe { Library::new(lib_path)? };
+
     let in_image_path = Path::new(env!("OUT_DIR")).join("data/ship.jpg");
     let out_image_path = Path::new(env!("OUT_DIR")).join("data/ship_inverted.jpg");
 
-    // Load the shared library (DLL)
-    let libcudaimg = unsafe { Library::new(lib_path)? };
+    let image = image::open(&in_image_path)?;
 
-    // Get the invertImage function from the library
-    let process_image: Symbol<InvertImageFn> = unsafe { libcudaimg.get(b"invertImage\0")? };
-
-    // Load the image using the image crate
-    let img = image::open(in_image_path)?;
-    let img_rgb8 = img.to_rgb8();
-    let mut img_asbytes = img_rgb8.as_raw().to_owned();
-    let width: u32 = img_rgb8.width();
-    let height: u32 = img_rgb8.height();
-
-    println!("Image width: {}, height: {}", width, height);
-
-    // Call the processImage function (invert the image)
-    unsafe {
-        // Note: the width * 3 is used because the image is in RGB format, which means that each pixel has 3 bytes (R, G, B)
-        process_image(
-            img_asbytes.as_mut_ptr(),
-            img_asbytes.len() as u32,
-            width * 3,
-            height,
-        );
-    }
+    let inverted_image = callcuda_rs::invert_image(&libcudaimg, &image)?;
 
     // Save the modified image
-    let img = image::DynamicImage::ImageRgb8(
-        image::RgbImage::from_raw(width, height, img_asbytes)
-            .expect("Failed to create the modified image from bytes"),
-    );
-
-    img.save(&out_image_path)
+    inverted_image
+        .save(&out_image_path)
         .expect("Failed to save the modified image");
-
     println!("Image inverted and saved to {:?}", out_image_path);
-    // The Library will be automatically unloaded when it goes out of scope
 
+    // The Library will be automatically unloaded when it goes out of scope
     Ok(())
 }
