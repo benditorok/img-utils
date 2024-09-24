7a772b1986b0b710213e4fa41bcefe866042f869
Bendegúz Török <bendi.torok@gmail.com>
2024-09-24 21:04:31 +0200

    Update the build process

--- build.rs
+++ build.rs
@@ -1,20 +1,29 @@
 use fs_extra::copy_items;
 use fs_extra::dir::CopyOptions;
 use std::env;
-use std::fs;
-use std::path::Path;
+use std::path::PathBuf;
 
 fn main() -> anyhow::Result<()> {
-    println!("cargo:rerun-if-changed=data/*");
-
     // Prepare what to copy and how
     let mut copy_options = CopyOptions::new();
     copy_options.overwrite = true;
     let paths_to_copy = vec!["data"];
 
-    // Copy the items to the directory where the executable will be built
-    let out_dir = env::var("OUT_DIR")?;
-    copy_items(&paths_to_copy, out_dir, &copy_options)?;
+    // Determine the profile (debug or release)
+    let profile = env::var("PROFILE")?; // Will be "debug" or "release"
+
+    // Construct the path to the target directory
+    let mut target_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
+    target_dir.push("target");
+    target_dir.push(&profile); // target/debug or target/release
+
+    // Create the target directory if it doesn't exist
+    std::fs::create_dir_all(&target_dir)?;
+
+    // Copy the items to the directory where the executable will be placed
+    copy_items(&paths_to_copy, &target_dir, &copy_options)?;
+
+    println!("cargo:rerun-if-changed=data/*"); // Ensure the build script runs if data changes
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 use libloading::Library;
-use std::{env, path::Path};
+use std::path::Path;
 
 fn main() -> anyhow::Result<()> {
     // Load the libcudaimg library
-    let lib_path = Path::new(env!("OUT_DIR")).join("data/libcudaimg.dll");
+    let lib_path = Path::new("data/libcudaimg.dll");
     let libcudaimg = unsafe { Library::new(lib_path)? };
+    let base_path = std::env::current_dir()?;
 
-    let in_image_path = Path::new(env!("OUT_DIR")).join("data/ship.jpg");
-    let out_image_path = Path::new(env!("OUT_DIR")).join("data/ship_inverted.jpg");
+    let in_image_path = base_path.join("data/ship.jpg");
+    let out_image_path = base_path.join("data/ship_inverted.jpg");
 
     let image = image::open(&in_image_path)?;
-
     let inverted_image = callcuda_rs::invert_image(&libcudaimg, &image)?;
 
     // Save the modified image
     inverted_image
         .save(&out_image_path)
         .expect("Failed to save the modified image");
-    println!("Image inverted and saved to {:?}", out_image_path);
+    println!("Image inverted and saved to {:?}", &out_image_path);
 
     // The Library will be automatically unloaded when it goes out of scope
     Ok(())
