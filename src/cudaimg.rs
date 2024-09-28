77c8ca11460bef997a048d847bccdbab60391c69
Bendegúz Török <bendi.torok@gmail.com>
2024-09-28 19:06:41 +0200

    Export image conversion

--- src/cudaimg.rs
+++ src/cudaimg.rs
@@ -1,35 +1,32 @@
 use image::DynamicImage;
 use libloading::{Library, Symbol};
 
+/// Definition of the processImage function from libcudaimg
+type InvertImageFn = unsafe extern "C" fn(image: *mut u8, image_len: u32, width: u32, height: u32);
+
 pub fn invert_image(libcudaimg: &Library, image: &DynamicImage) -> anyhow::Result<DynamicImage> {
-    // Define the signature for the processImage function
-    type InvertImageFn =
-        unsafe extern "C" fn(image: *mut u8, image_len: u32, width: u32, height: u32);
     // Get the invertImage function from the library
     let process_image: Symbol<InvertImageFn> = unsafe { libcudaimg.get(b"invertImage\0")? };
 
-    // Load the image using the image crate
-    let img_rgb8 = image.to_rgb8();
-    let mut img_asbytes = img_rgb8.as_raw().to_owned();
-    let width: u32 = img_rgb8.width();
-    let height: u32 = img_rgb8.height();
+    // Get the image data
+    let mut img = crate::get_image_data(&image);
 
-    println!("Image width: {}, height: {}", width, height);
+    println!("Image width: {}, height: {}", img.width, img.height);
 
     // Call the processImage function (invert the image)
     unsafe {
         // Note: the width * 3 is used because the image is in RGB format, which means that each pixel has 3 bytes (R, G, B)
         process_image(
-            img_asbytes.as_mut_ptr(),
-            img_asbytes.len() as u32,
-            width * 3,
-            height,
+            img.bytes.as_mut_ptr(),
+            img.raw_len,
+            img.width * img.pixel_size,
+            img.height,
         );
     }
 
     // Create a new image from the modified bytes
     let inverted_image = image::DynamicImage::ImageRgb8(
-        image::RgbImage::from_raw(width, height, img_asbytes)
+        image::RgbImage::from_raw(img.width, img.height, img.bytes)
             .expect("Failed to create the modified image from bytes"),
     );
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,34 @@ use image::DynamicImage;
 use libloading::{Library, Symbol};
 
 pub mod cudaimg;
+
+pub struct ImageData {
+    pub bytes: Vec<u8>,
+    pub raw_len: u32,
+    pub width: u32,
+    pub height: u32,
+    pub pixel_size: u32,
+}
+
+/// Get the image data from a DynamicImage
+///
+/// # Arguments
+///
+/// * `image` - The DynamicImage to get the data from
+///
+/// # Returns
+///
+/// * An ImageData struct containing the image data
+pub fn get_image_data(image: &DynamicImage) -> ImageData {
+    let img_rgb8 = image.to_rgb8();
+    let bytes = img_rgb8.as_raw().to_owned();
+    let raw_len = bytes.len() as u32;
+
+    ImageData {
+        bytes,
+        raw_len,
+        width: img_rgb8.width(),
+        height: img_rgb8.height(),
+        pixel_size: 3, // RGB format (3 bytes per pixel)
+    }
+}
